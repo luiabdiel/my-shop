d7d797de7efbd2efdd5bbcfadca4fcaa5f71259e
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-03-06 23:06:15 -0300

    feat: application header

---
+++ src/styles/pages/app.ts
@@ -0,0 +1,17 @@
+import { styled } from "..";
+
+export const Container = styled('div', {
+  display: 'flex',
+  alignItems: 'flex-start',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  minHeight: '100vh',
+})
+
+
+export const Header = styled('header', {
+  padding: '2rem 0',
+  width: '100%',
+  maxWidth: 1180,
+  margin: '0 auto',
+})
